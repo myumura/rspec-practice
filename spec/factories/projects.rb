f09ca6231c23b9e76f207d20ab8259cd727d11f7
Mifuka Yumura <mifukuma-1st-323@outlook.jp>
2021-07-18 20:05:04 +0900

    Revert "第五章"
    This reverts commit 3493153a1b52452663b815a3738eeb9d611d9377.

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -5,10 +5,6 @@ FactoryBot.define do
     due_on 1.week.from_now
     association :owner
 
-    trait :invalid do
-      name nil
-    end
-
     trait :with_notes do
       after(:create) { |project| create_list(:note, 5, project: project) }
     end
